65ebd25ab644d26b93b586b8a34b3a40cff01c7b
Nguyen Pham <nguyen.phamvan@vti.com.vn>
2025-04-01 11:22:59 +0700

    feat: add Kong module and service for API gateway integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
-import { CoreModule } from './modules/core/core.module';
 import { RoleModule } from './modules/role/role.module';
+import { CoreModule } from './shared/core/core.module';
+import { RabbitMqModule } from './shared/core/transporter/rabbitmq/rabbitmq.module';
 import { GraphQLModule } from './shared/graphql';
 import { PrismaModule } from './shared/prisma';
 import config from '@/common/configs/config';
@@ -14,8 +15,9 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    CoreModule,
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+    RabbitMqModule,
+    CoreModule,
     ScheduleModule.forRoot(),
     PrismaModule,
     GraphQLModule,
--- src/shared/core/core.module.ts
+++ src/shared/core/core.module.ts
@@ -1,7 +1,9 @@
 import { CoreControler } from './core.controller';
 import { ExceptionInterceptor } from './interceptors/exception.interceptor';
 import { TransformInterceptor } from './interceptors/transftorm.interceptor';
+import { KongModule } from './kong/kong.module';
 import { ValidationPipe } from './pipes/validation.pipe';
+import { KafkaModule } from './transporter/kafka/kafka.module';
 import { RabbitMqModule } from './transporter/rabbitmq/rabbitmq.module';
 // import { RabbitMqModule } from './transporter/rabbitmq/rabbitmq.module';
 import { RabbitMqService } from './transporter/rabbitmq/rabbitmq.service';
@@ -10,7 +12,7 @@ import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 @Global()
 @Module({
-  imports: [],
+  imports: [KafkaModule, KongModule],
   providers: [
     {
       provide: APP_INTERCEPTOR,
@@ -26,6 +28,6 @@ import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
     },
   ],
   controllers: [CoreControler],
-  exports: [],
+  exports: [KongModule],
 })
 export class CoreModule {}
